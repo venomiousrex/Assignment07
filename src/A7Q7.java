199ecddbd31bd98d25f3f580dde56365b5ac4712
Administrator <Administrator@n-9c4c592af9fe4>
2015-11-27 07:22:32 -0500

    They're history, without a trace

--- src/A7Q7.java
+++ src/A7Q7.java
@@ -21,12 +21,17 @@ public class A7Q7 {
         // Plan is to find first multiples of numbers < 10 and cross out those multiples over 10, then print out whats left.
         Scanner in = new Scanner(System.in);
         
+        int zero = 0;
         int[] array = new int [1000];
         for(int i = 2; i < 13; i++){
         for(int p = 0; p <1001; p = p + i){
-            array[i] =  
-            if(i <= 10){
-               
+            
+            if(p <= 10 ){
+                System.out.println(p);
+            }
+                    
+            if(p >= 10){
+               array[i] = zero;
             }
              
         }
